2badcd68988bd5493661a9d45cfb1a6a222a8bbc
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2019-10-25 13:34:42 -0400

    bumped up transit-clj

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject luminus-transit "0.1.1"
+(defproject luminus-transit "0.1.2"
   :description "extended transit types"
   :url "https://github.com/luminus-framework/luminus-transit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.andrewmcveigh/cljs-time "0.5.2"]
-                 [com.cognitect/transit-clj "0.8.313"]
+                 [com.cognitect/transit-clj "0.8.319"]
                  [com.cognitect/transit-cljs "0.8.256"]])
